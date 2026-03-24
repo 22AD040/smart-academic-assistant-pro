f6d72e8850ce76fb2b4378c209982b96ebd56c1e
Ratchita B <ratchita621@gmail.com>
2026-03-25 04:18:34 +0530

    fixing main.py

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
     return {"status": "ok", "message": "Backend is running"}
+
+# 🔥 ADD THIS
+@app.head("/")
+def head_root():
+    return {"status": "ok"}
